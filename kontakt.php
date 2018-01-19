fbded832705dbec1f98c7212b93939b34423dbfa
Grzegorz Dzien <gdzien@gmail.com>
2018-01-19 21:11:59 +0000

    fixed bootstrap in contact

--- kontakt.php
+++ kontakt.php
@@ -8,7 +8,9 @@
           <div class="row">        
         <div class="col-md-12">
           <div class="card text-center" style="opacity: .5; padding: 10px;">
-              Aby się z nami skontaktować połącz się z serwerem dziecisieci.org.pl za pomocą <a href="http://teamspeak.com/en/downloads.html#client" class="alert-link">Klienta TeamSpeak</a>.
+          <p class="card-text">
+              Aby się z nami skontaktować połącz się z serwerem dziecisieci.org.pl za pomocą <a href="http://teamspeak.com/en/downloads.html#client">Klienta TeamSpeak</a>.
+          </p>
           </div>
         </div>
       </div>
