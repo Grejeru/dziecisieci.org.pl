07ff65686370373fc54a96340702e5ecd5ec511f
Grzegorz Dzien <gdzien@gmail.com>
2018-01-19 20:38:13 +0000

    Usunieto kartofla, dodano lamberta

--- ekipa.php
+++ ekipa.php
@@ -32,7 +32,7 @@
           <div class="card col-md-4" style="width: 28rem; opacity: .7;">
             <img class="card-img-top" src="/zoltan_sog-profile_image-b443037aaf613e4e-300x300.jpeg" alt="Zoltan_SOG">
             <div class="card-body">
-              <h5 class="card-title"><a href="https://www.twitch.tv/zoltan_sog/">Zoltan_SOG</a></h5>
+              <h5 class="card-title"><img src="/twitch.png" alt="Zoltan_SOG"> Zoltan_SOG</h5>
               <p class="card-text">
               Wreszcie 40 na karku i cały czas gram w gry komputerowe.<br /><br />
               Swoją przygodę rozpocząłem inwestując otrzymane pieniądze z okazji Komunii Św. w komputer ATARI 800XL (alternatywą był zestaw LEGO). Wygrał komputer i do tej pory moja praca i zabawa jest związana z komputerami.<br /><br />
@@ -44,8 +44,8 @@
           <div class="card col-md-4" style="width: 28rem; opacity: .7;">
             <img class="card-img-top" src="/95e46461e5ff5245-profile_image-300x300.png" alt="SuperMelepeta">
             <div class="card-body">
-              <h5 class="card-title"><a href="https://www.twitch.tv/supermelepeta">SuperMelepeta</a></h5>
-              <p class="card-text"><strong>Melepeta</strong> - potocznie pejoratywne określenie kogoś niezdarnego, powolnego, niemądrego. 
+            <h5 class="card-title"><img src="/twitch.png" alt="supermelepeta"> SuperMelepeta</h5>
+            <p class="card-text"><strong>Melepeta</strong> - potocznie pejoratywne określenie kogoś niezdarnego, powolnego, niemądrego. 
                 <br />Tak jak SuperMan, miał być wszystkim co najlepsze w człowieku, tak SuperMelepeta pokazuje Super wersję cech melepety. Nie trafia w przeciwników, nie znajduje przedmiotów. Nie wie skąd do niego srztelają i je kolację z kurczaka tylko jak ktoś inny ją ufunduje ;-) Zwykle ginie na początku rundy.</p>
             </div>
           </div>
@@ -55,7 +55,7 @@
           <div class="card col-md-4" style="width: 28rem; opacity: .7;">
             <img class="card-img-top" src="/519UvPjYXzL._SY300_.jpg" alt="Ciastek">
             <div class="card-body">
-              <h5 class="card-title"><a href="https://www.twitch.tv/cisakus">Ciastek</a></h5>
+              <h5 class="card-title"><img src="/twitch.png" alt="cisakus"> cisakus</h5>
               <p class="card-text">
               Bezrobotny węglokop. <br />
               Niespełniony pisarz Sci-Fi. Leczy się na nogi, bo na głowę już za późno. Kocha jeździć pociągami. <br /><br />
@@ -67,7 +67,7 @@
           <div class="card col-md-4" style="width: 28rem; opacity: .7;">
             <img class="card-img-top" src="/175867ce87e4b45f-profile_image-300x300.jpeg" alt="WinneRek">
             <div class="card-body">
-              <h5 class="card-title"><a href="https://www.twitch.tv/winnerek">W!nneR</a></h5>
+              <h5 class="card-title"><img src="/twitch.png" alt="Winnerek"> Winnerek</h5>
               <p class="card-text">
               Jestem Szymon, mam 18 lat i dopiero zaczynam przygodę ze streamowaniem. Jest to moje nowe hobby :D<br /><br />
               Głównie gram w cs'a, ale również grywam w inne gierki fps, mmo/rpg, także zapraszam Wszystkich zainteresowanych na transmisje, które odbywają się kilka razy w tygodniu, głownie wieczorami. :)
@@ -78,10 +78,10 @@
           <div class="card col-md-4" style="width: 28rem; opacity: .7;">
             <img class="card-img-top" src="/585d9bd6-6f00-47fb-8464-0ecbf8e31e6e-profile_image-300x300.png" alt="Kartofel321123">
             <div class="card-body">
-              <h5 class="card-title"><a href="https://www.twitch.tv/kartofel321123">Kartofel321123</a></h5>
-              <p class="card-text">
-               Młody ale zdolny. Śmieszek.<br />
-               Ulubione powiedzenie: iks-de<br />
+            <h5 class="card-title"><img src="/twitch.png" alt="Lambert"> Lambert</h5>
+            <p class="card-text">
+               ムᛚᛆᛗᛒᛖᚱᛏム<br />
+               
               </p>
             </div>
           </div>
