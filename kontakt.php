373a550fbe3a4d23571eb957bee59c88ffa62b7b
Grzegorz Dzien <gdzien@gmail.com>
2018-01-19 20:59:23 +0000

    Updated contact page

--- kontakt.php
+++ kontakt.php
@@ -8,7 +8,7 @@
           <div class="row">        
         <div class="col-md-12">
           <div class="card text-center" style="opacity: .5; padding: 10px;">
-              Aby się z nami skontaktować połącz się z serwerem dziecisieci.org.pl za pomocą Klienta TeamSpeak.
+              Aby się z nami skontaktować połącz się z serwerem dziecisieci.org.pl za pomocą <a href="http://teamspeak.com/en/downloads.html#client">Klienta TeamSpeak</a>.
           </div>
         </div>
       </div>
